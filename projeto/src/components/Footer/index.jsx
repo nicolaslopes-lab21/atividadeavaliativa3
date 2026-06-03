fc230a361cd853cab99fed73416eb52ec8c6609f
nicolaslopes-lab21 <nicolas.lopes@ba.estudante.senai.br>
2026-06-02 21:31:40 -0300

    Add files via upload

---
+++ projeto/src/components/Footer/index.jsx
@@ -0,0 +1,13 @@
+import "./style.css";
+
+function Footer() {
+    return (
+    <footer className="footer">
+        <p>Desenvolvido por Nicolas De Souza Silva Lopes</p>
+        <p>"Família é onde a vida começa e o amor nunca termina."</p>
+        <p>© 2025 Todos os direitos reservados.</p>
+    </footer>
+    );
+}
+
+export default Footer;
---
+++ projeto/src/components/Header/index.jsx
@@ -0,0 +1,19 @@
+import "./style.css";
+
+function Header() {
+	return (
+    <header className="header">
+        <h1> Homenagem aos Meus Pais </h1>
+
+        <nav>
+        <a href="#inicio">Início</a>
+        <a href="#mae">Mãe</a>
+        <a href="#pai">Pai</a>
+        <a href="#galeria">Galeria</a>
+        <a href="#videos">Vídeos</a>
+        </nav>
+    </header>
+);
+}
+
+export default Header;
